330498c2ecd5ef7e11b9338c4c447a3f588202fe
Cubilla <cubilladev@gmail.com>
2024-02-21 19:20:44 -0300

    Cambios a enpoint

--- server/src/routers/add.product.controller.js
+++ server/src/routers/add.product.controller.js
@@ -2,7 +2,7 @@ const express = require('express')
 const routerAddProduct = express.Router()
 const product = require('../controller/product.controller')
 
-routerAddProduct.post('/addproduct/', product.addProduct);
+routerAddProduct.post('/addproduct', product.addProduct);
 
 
 module.exports = routerAddProduct;
